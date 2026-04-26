583f0b74015585e6eec9c39f90761921d870ccce
sam-goodwin0-0 <jane25.goodwin@gmail.com>
2026-04-26 20:48:14 +0200

    menu version 2

--- app/src/main/java/com/example/lendahand/menuActivity.java
+++ app/src/main/java/com/example/lendahand/menuActivity.java
@@ -19,7 +19,7 @@ public class menuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_sign_up_page);
+        setContentView(R.layout.menu);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
