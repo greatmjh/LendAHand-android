1abcf6d5dc41ab8148aa3255b9854280ac227a35
greatmjh <66207287+greatmjh@users.noreply.github.com>
2026-05-03 18:54:32 +0200

    Initial OkHttp setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,7 @@ dependencies {
     implementation(libs.navigation.fragment)
     implementation(libs.navigation.ui)
     implementation(libs.recyclerview)
+    implementation(libs.okhttp)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
